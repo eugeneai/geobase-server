aa55017799709b0f5298cb00a79736b7e26503dc
Evgeny Cherkashin <eugeneai@irnok.net>
2016-06-13 20:30:23 +0800

    The first version of conversion

--- geobase_run.pl
+++ geobase_run.pl
@@ -13,3 +13,5 @@ sandbox:safe_primitive(geobase:geobase(_,_,_)).
 
 :- geobase:loaddba.
 :- icc:create_graph(geob).
+:- icc:create_graph(geodata).
+:- rdf_load('data/activity_fall_data.ttl',[graph(geodata)]).
